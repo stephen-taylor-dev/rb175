a205ae6838bc97523094a0d036df781fcbdd7124
Stephen Taylor <stephenmtaylor93@gmail.com>
2025-01-21 17:37:27 -0500

    getting started

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubi"
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+  erb :chapter
+end
